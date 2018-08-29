f037ef0055d30e3ee388265d521876f4e817aa94
Ricardo Lynch <rlynch@Ricardos-iMac.local>
2018-08-29 09:45:03 -0500

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  dateval: string = null;
+  dateSelected(val) {
+    this.dateval = val;
+  }
 }
